c291f97aed1c13bc4a19a3936f52958a21f5bd76
Term-inator <1121745627@qq.com>
2020-10-18 22:57:56 +0800

    20-10-18

--- www/js/data.js
+++ www/js/data.js
@@ -4,30 +4,28 @@ var data = {
 }
 var things_buffer = []; //things的缓冲区，用于排序
 
-function sort_by_ddl() {
+function sortByDdl() {
     things_buffer.sort(function(a, b) { //按ddl排序
         if(a.ddl.year != b.ddl.year) {
             return a.ddl.year - b.ddl.year;
         }
 
-        if(a.ddl.month == '') {
-            a.ddl.month = 50; //为空则排在12后
-        }
-        if(b.ddl.month == '') {
-            b.ddl.month = 50; //为空则排在12后
-        }
-
-        if(a.ddl.date == '') {
-            a.ddl.date = 50; //为空则排在31后
-        }
-        if(b.ddl.month == '') {
-            b.ddl.month = 50; //为空则排在31后
-        }
-
         if(a.ddl.month != b.ddl.month) {
+            if(a.ddl.month == '') {
+                return 1;
+            }
+            if(b.ddl.month == '') {
+                return -1;
+            }
             return a.ddl.month - b.ddl.month;
         }
         if(a.ddl.date != b.ddl.date) {
+            if(a.ddl.date == '') {
+                return 1;
+            }
+            if(b.ddl.date == '') {
+                return -1;
+            }
             return a.ddl.date - b.ddl.date;
         }
         return a.thing - b.thing;
@@ -38,7 +36,7 @@ function sort_by_ddl() {
 function loadUI() {
     if(data.ddl_sorted) { //上次要求按ddl排序则延续该设置
         $('#ddl').addClass("head-active");
-        sort_by_ddl();
+        sortByDdl();
         things_buffer.forEach(e => {
             addThings(e.thing, e.ddl.year, e.ddl.month, e.ddl.date, e.position.x, e.position.y);
         })
